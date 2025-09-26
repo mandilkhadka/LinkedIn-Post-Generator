38e5cbf0d4cb3a74ef02d02840aaf50d4c577049
mandilkhadka <khadkamandil62@gmail.com>
2025-09-26 15:16:24 +0900

    ai working

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.generated_text = RubyLLM.chat.ask("Generate a Linkedin post from this text: #{@post.notes} with all of the critereia mentioned
+    1. Tone = #{@post.tone}
+    2. Length = #{@post.length_option}
+    3. Include hastags? = #{@post.hashtags}").content
+      if @post.save
+        redirect_to post_path(@post), notice: "Post was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:notes, :tone, :length_option, :hashtags)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,3 @@
 class Post < ApplicationRecord
   validates :notes, presence: true
-  
-  TONES = %w[professional friendly witty inspirational]
-  LENGTHS = %w[short medium long]
 end
